6ada5d89f1b1a81cd98fa1d231777540c7dc5ee0
Vojislav <vojislav.stupar@gmail.com>
2019-06-28 20:57:10 +0200

    zad3 dodavanje customer forma

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="container">
+        <div>
+            <form @submit.prevent="addCustomer">
+                <div class="form-group">
+                    <label for="firstName">First Name:</label>
+                    <input type="text" class="form-control" id="firstName" v-model="newCustomer.firstName" required/>
+                </div>
+                <div class="form-group">
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" class="form-control" id="lastName" v-model="newCustomer.lastName" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" v-model="newCustomer.email" required>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit">Add Customer</button>
+                </div>
+            </form>
+        </div>
+        <div>
         <h3>Customer list</h3>
         <table class="table-striped">
             <tr v-for="(customer,index) in customers" :key="index">
@@ -9,6 +29,7 @@
                 <td><button v-on:click = "deleteCustomer(index)">Delete Customer</button></td>
             </tr>
         </table>
+        </div>
     </div>
 </template>
 
@@ -16,6 +37,11 @@
 export default {
   data () {
       return {
+          newCustomer: {
+              firstName: '',
+              lastName: '',
+              email: '',
+          },
           customers: [
               {id:1,firstName:'Zoran', lastName:'Hajduk', email:'zoran@gmail.com'},
               {id:2,firstName:'Pera', lastName:'Peric', email:'peraperic@gmail.com'},
@@ -27,6 +53,14 @@ export default {
   methods: {
       deleteCustomer (index) {
           this.customers.splice(index,1)
+      },
+      addCustomer () {
+          this.customers.push({...this.newCustomer})
+          this.newCustomer = {
+              firstName: '',
+              lastName: '',
+              email: '',
+          }
       }
   }
 }
